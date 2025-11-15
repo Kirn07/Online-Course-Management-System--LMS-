13063c89de1ff947efcfe958e6ab3588c3b375c1
Kiran Kushwaha <168515559+Kirn07@users.noreply.github.com>
2025-11-15 15:14:39 +0530

    Remove unused import for GetMapping

--- backend/src/main/java/com/lms/dev/controller/WebController.java
+++ backend/src/main/java/com/lms/dev/controller/WebController.java
@@ -1,8 +1,8 @@
 package com.lms.dev.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequestMapping("/web")
 public class WebController {
